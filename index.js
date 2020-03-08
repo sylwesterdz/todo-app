f955a3f6bce48d7e100e4e39cc3cfed5902680cd
sylwesterdz <sylwesterdz@gmail.com>
2020-03-08 22:08:08 +0100

    Array-submittodo

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-let mess = "Hello"
-alert (mess);
+let todos = []
+
+const submittodo = document.getElementById("submittodo");
+
+submittodo.addEventListener('click', function(event){
+    event.preventDefault();
+    const todotext = document.getElementById("todo").value;
+    todos.push(todotext)
+    document.getElementById("todo").value = ""
+    console.log(todos);
+})
