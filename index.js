946dfae700d44a49530aeb468361723a4db9846a
sylwesterdz <sylwesterdz@gmail.com>
2020-03-17 22:12:09 +0100

    Add appendTodo

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-let todos = []
-
-const submittodo = document.getElementById("submittodo");
+let todos = [];
+const submittodo = document.querySelector("#submittodo");
+const todoList = document.querySelector(".todo-list");
 
 submittodo.addEventListener('click', function(event){
     event.preventDefault();
-    const todotext = document.getElementById("todo").value;
-    todos.push(todotext)
-    document.getElementById("todo").value = ""
-    console.log(todos);
-})
+
+    const todotext = document.querySelector("#todo").value;
+    
+    todos.push(todotext);
+    document.querySelector("#todo").value = "";
+    appendTodo(todotext);      
+});
+
+function appendTodo(text){
+    const listElement = document.createElement("li");
+    const todotext = document.createTextNode(text);
+    
+    listElement.appendChild(todotext);
+    todoList.appendChild(listElement);
+}
