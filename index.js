304ab19cd2beacbe0e94b92038d8b3a6697e9075
sylwesterdz <sylwesterdz@gmail.com>
2020-03-23 21:24:15 +0100

    Add Delete button

--- index.js
+++ index.js
@@ -13,14 +13,20 @@ submittodo.addEventListener('click', function(event){
 });
 
 function appendTodo(text){
-    todoList.innerHTML += `<li class="list-element">${text} <button class="todo-done">Done</button> </li>`;
+    todoList.innerHTML += `<li class="list-element">${text} <button class="todo-done">Done</button> <button class="todo-delete">Delete</button> </li>`;
     
     const todoDone = document.querySelectorAll(".todo-done");
     const listElement = document.querySelectorAll(".list-element");
+    const todoDelete = document.querySelectorAll(".todo-delete");
 
     todoDone.forEach((button, index) => {
         button.addEventListener('click', () => {
             listElement[index].classList.add("done");
         });
-    });   
+    });
+    todoDelete.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            listElement[index].remove();
+        });
+    });
 };
